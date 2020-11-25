6f7df35fe8d5330388cee0609fc0f449234941be
Garrett Walter <gtwalter150@gmail.com>
2020-11-24 22:57:49 -0500

    added inital promt and cases

--- employee.js
+++ employee.js
@@ -17,20 +17,69 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    
+    runSearch();
   });
 
-//   function runSearch() {
-//     inquirer
-//       .prompt({
-//         name: "action",
-//         type: "rawlist",
-//         message: "What would you like to do?",
-//         choices: [
-//           "Find songs by artist",
-//           "Find all artists who appear more than once",
-//           "Find data within a specific range",
-//           "Search for a specific song",
-//           "Find artists with a top song and top album in the same year"
-//         ]
-//       })
+  function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+          "View all employees",
+          "View all employees by department",
+          "View all employees by manager",
+          "Add employee",
+          "Remove employee",
+          "Update employee role",
+          "Update employee manager",
+          "Add department",
+          "Add manager",
+          "Add role"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "View all employees":
+          artistSearch();
+          break;
+  
+        case "View all employees by department":
+          multiSearch();
+          break;
+  
+        case "View all employees by manager":
+          rangeSearch();
+          break;
+  
+        case "Add employee":
+          songSearch();
+          break;
+  
+        case "Remove employee":
+          songAndAlbumSearch();
+          break;
+
+        case "Update employee role":
+          songAndAlbumSearch();
+          break;
+
+        case "Update employee manager":
+          songAndAlbumSearch();
+          break;
+
+        case "Add department":
+          songAndAlbumSearch();
+          break;
+
+        case "Add manager":
+          songAndAlbumSearch();
+          break;
+
+        case "Add role":
+          songAndAlbumSearch();
+          break;
+        }
+      });
+  }
